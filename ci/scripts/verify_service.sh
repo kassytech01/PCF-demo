9680ce29a27f4f1d9a7a0875817f4a4a1ee25b54
bp-tkasuga <tkasuga@nikkeibp.co.jp>
2018-04-11 21:50:01 +0900

    modify

--- ci/scripts/verify_service.sh
+++ ci/scripts/verify_service.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-result=$(curl -LI localhost:8080 -o /dev/null -w '%{http_code}\n' -s)
+result=$(curl -LI localhost:80/pcf-demo/ -o /dev/null -w '%{http_code}\n' -s)
 
 if [[ "$result" =~ "200" ]]; then
     exit 0
