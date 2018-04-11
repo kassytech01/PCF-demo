498ed6e767d7e3747af410abdf3668ea8acecc44
bp-tkasuga <tkasuga@nikkeibp.co.jp>
2018-04-11 22:16:18 +0900

    modify

--- ci/scripts/verify_service.sh
+++ ci/scripts/verify_service.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-result=$(curl -LI localhost:80/pcf-demo/ -o /dev/null -w '%{http_code}\n' -s)
+function verify_service(){
+    result=$(curl -LI localhost:80/pcf-demo/ -o /dev/null -w '%{http_code}\n' -s)
 
-if [[ "$result" =~ "200" ]]; then
-    exit 0
-else
-    exit 1
-fi
+    if [[ "$result" =~ "200" ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+NEXT_WAIT_TIME=0
+until verify_service || [ $NEXT_WAIT_TIME -eq 10 ]; do
+   sleep $(( NEXT_WAIT_TIME++ ))
+   echo $NEXT_WAIT_TIME
+done
